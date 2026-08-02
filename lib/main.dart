737e16827f59ef19ca9c408c8329b11250264b4e
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-02 16:05:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'Provider/barber_data_from_map_provider.dart';
 import 'Provider/map_data_provider.dart';
-import 'screens/AppointmentScreen.dart';
-import 'screens/bookingScreen.dart';
-import 'screens/categoryScreen.dart';
 import 'screens/mapsScreen.dart';
+import 'screens/bookingScreen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -20,91 +16,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider<BarberDataFromMapProvider>(
+          create: (_) => BarberDataFromMapProvider()..loadData(),
+        ),
         ChangeNotifierProvider<MapDataProvider>(
-          create: (_) => MapDataProvider(),
+          create: (_) => MapDataProvider()..loadMapData(),
         ),
       ],
       child: MaterialApp(
-        title: "Assam Multi Service",
-        debugShowCheckedModeBanner: false,
+        title: 'Assam Local Service',
         theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.blueGrey[900],
-          colorScheme: ColorScheme.dark(
-            primary: Colors.blueAccent,
-            secondary: Colors.cyan[600]!,
-            surface: Colors.blueGrey[900]!,
-          ),
-          scaffoldBackgroundColor: Colors.blueGrey[900],
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-          ),
+          primarySwatch: Colors.blue,
         ),
+        initialRoute: '/',
         routes: {
-          '/category': (context) => const CategoryScreen(),
+          '/': (context) => const Mappage(),
           '/mappage': (context) => const Mappage(),
           '/bookingScreen': (context) => const BookingScreen(),
-          '/appointmentScreen': (context) => const AppointmentScreen(),
         },
-        home: const SplashScreen(),
-      ),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(const Duration(seconds: 3), () {
-      // Splash screen ke baad Category Screen par navigate hoga
-      Navigator.of(context).pushReplacementNamed('/category');
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey[900],
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          const Icon(
-            Icons.handyman,
-            size: 100,
-            color: Colors.blueAccent,
-          ),
-          Column(
-            children: const [
-              Text(
-                "Assam All-in-One Services",
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                "Electrician, Plumber, Carpenter, Barber & More",
-                style: TextStyle(color: Colors.white60, fontSize: 13),
-              ),
-            ],
-          ),
-          const CircularProgressIndicator(
-            strokeWidth: 3,
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.blueAccent),
-          )
-        ],
       ),
     );
   }
