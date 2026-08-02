50612b066809c95e083bf80c065bb630e0f5892a
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-02 17:18:06 +0530

    Create appointmentScreen.dart

---
+++ lib/screens/appointmentScreen.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+
+class AppointmentScreen extends StatefulWidget {
+  const AppointmentScreen({Key? key}) : super(key: key);
+
+  @override
+  _AppointmentScreenState createState() => _AppointmentScreenState();
+}
+
+class _AppointmentScreenState extends State<AppointmentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.orangeAccent[100],
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text("Appointment"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.notifications,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.sort,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Card(
+                  elevation: 10,
+                  color: Colors.orangeAccent[100],
+                  margin: const EdgeInsets.all(0),
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          const Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    "John Doe ",
+                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                                  ),
+                                  Icon(Icons.done, size: 15)
+                                ],
+                              ),
+                              SizedBox(height: 5),
+                              Text("Assam, India", style: TextStyle(fontSize: 14)),
+                              SizedBox(height: 5),
+                              Row(
+                                children: <Widget>[
+                                  for (int i = 0; i < 5; i++)
+                                    Icon(Icons.star, size: 17)
+                                ],
+                              ),
+                              SizedBox(height: 7),
+                            ],
+                          ),
+                          const SizedBox(width: 50),
+                          IconButton(
+                            icon: const Icon(Icons.share),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.favorite),
+                            onPressed: () {},
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const SizedBox(height: 40),
+                      const Text(
+                        "Available Timings",
+                        style: TextStyle(color: Colors.black54, fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(width: 55, color: Colors.black54, height: 3),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        height: 110,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 5,
+                          itemBuilder: (BuildContext ctxt, int index) {
+                            return Stack(
+                              children: <Widget>[
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: const SizedBox(height: 110, width: 110),
+                                ),
+                                Container(
+                                  height: 110,
+                                  width: 90,
+                                  decoration: BoxDecoration(
+                                    color: Colors.redAccent,
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                ),
+                                Container(
+                                  height: 110,
+                                  width: 90,
+                                  alignment: Alignment.center,
+                                  child: const Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(
+                                        '09:00',
+                                        style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+                                      ),
+                                      Text(
+                                        'AM',
+                                        style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Services",
+                        style: TextStyle(color: Colors.black54, fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(width: 55, color: Colors.black54, height: 3),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 4, 8, 2),
+                        child: Column(
+                          children: <Widget>[
+                            for (int i = 0; i < 2; i++)
+                              const Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(
+                                    "Service Charge",
+                                    style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.w600),
+                                  ),
+                                  Text(
+                                    "\u20B9 300",
+                                    style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.w600),
+                                  ),
+                                ],
+                              ),
+                            const SizedBox(height: 10),
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  "Total",
+                                  style: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.w600),
+                                ),
+                                Text(
+                                  "\u20B9 600",
+                                  style: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.w600),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(height: 15, color: Colors.grey),
+              ],
+            ),
+          ),
+          const Positioned(
+            left: 25,
+            top: 20,
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 55.0,
+              child: CircleAvatar(
+                radius: 48,
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
