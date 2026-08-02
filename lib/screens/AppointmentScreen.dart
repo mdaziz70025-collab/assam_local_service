507e19f82dd11c27fa314d9b83d5e40f6e55bd0d
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-02 16:47:23 +0530

    Update AppointmentScreen.dart

--- lib/screens/AppointmentScreen.dart
+++ lib/screens/AppointmentScreen.dart
@@ -50,59 +50,36 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
                   margin: const EdgeInsets.all(0),
                   child: Column(
                     children: <Widget>[
-                      const SizedBox(
-                        height: 15,
-                      ),
+                      const SizedBox(height: 15),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: <Widget>[
-                          Column(
+                          const Column(
                             mainAxisSize: MainAxisSize.min,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
-                              const Row(
+                              Row(
                                 children: <Widget>[
                                   Text(
                                     "John Doe ",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
+                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                                   ),
-                                  Icon(
-                                    Icons.done,
-                                    size: 15,
-                                  )
+                                  Icon(Icons.done, size: 15)
                                 ],
                               ),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              const Text(
-                                "Thane, India",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 5,
-                              ),
+                              SizedBox(height: 5),
+                              Text("Assam, India", style: TextStyle(fontSize: 14)),
+                              SizedBox(height: 5),
                               Row(
                                 children: <Widget>[
                                   for (int i = 0; i < 5; i++)
-                                    const Icon(
-                                      Icons.star,
-                                      size: 17,
-                                    )
+                                    Icon(Icons.star, size: 17)
                                 ],
                               ),
-                              const SizedBox(
-                                height: 7,
-                              ),
+                              SizedBox(height: 7),
                             ],
                           ),
-                          const SizedBox(
-                            width: 50,
-                          ),
+                          const SizedBox(width: 50),
                           IconButton(
                             icon: const Icon(Icons.share),
                             onPressed: () {},
@@ -121,27 +98,14 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      const SizedBox(
-                        height: 40,
-                      ),
+                      const SizedBox(height: 40),
                       const Text(
                         "Available Timings",
-                        style: TextStyle(
-                            color: Colors.black54,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
+                        style: TextStyle(color: Colors.black54, fontSize: 20, fontWeight: FontWeight.bold),
                       ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Container(
-                        width: 55,
-                        color: Colors.black54,
-                        height: 3,
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
+                      const SizedBox(height: 5),
+                      Container(width: 55, color: Colors.black54, height: 3),
+                      const SizedBox(height: 20),
                       SizedBox(
                         height: 110,
                         child: ListView.builder(
@@ -152,10 +116,7 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
                               children: <Widget>[
                                 ClipRRect(
                                   borderRadius: BorderRadius.circular(8),
-                                  child: const SizedBox(
-                                    height: 110,
-                                    width: 110,
-                                  ),
+                                  child: const SizedBox(height: 110, width: 110),
                                 ),
                                 Container(
                                   height: 110,
@@ -174,19 +135,11 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
                                     children: <Widget>[
                                       Text(
                                         '09:00',
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w500,
-                                        ),
+                                        style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
                                       ),
                                       Text(
                                         'AM',
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w500,
-                                        ),
+                                        style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
                                       ),
                                     ],
                                   ),
@@ -196,71 +149,43 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
                           },
                         ),
                       ),
-                      const SizedBox(
-                        height: 20,
-                      ),
+                      const SizedBox(height: 20),
                       const Text(
                         "Services",
-                        style: TextStyle(
-                            color: Colors.black54,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
+                        style: TextStyle(color: Colors.black54, fontSize: 20, fontWeight: FontWeight.bold),
                       ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Container(
-                        width: 55,
-                        color: Colors.black54,
-                        height: 3,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
+                      const SizedBox(height: 5),
+                      Container(width: 55, color: Colors.black54, height: 3),
+                      const SizedBox(height: 10),
                       Padding(
                         padding: const EdgeInsets.fromLTRB(8, 4, 8, 2),
                         child: Column(
                           children: <Widget>[
                             for (int i = 0; i < 2; i++)
                               const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: <Widget>[
                                   Text(
-                                    "Afro Haircut",
-                                    style: TextStyle(
-                                        color: Colors.grey,
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w600),
+                                    "Service Charge",
+                                    style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.w600),
                                   ),
                                   Text(
                                     "\u20B9 300",
-                                    style: TextStyle(
-                                        color: Colors.grey,
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w600),
+                                    style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.w600),
                                   ),
                                 ],
                               ),
-                            const SizedBox(
-                              height: 10,
-                            ),
+                            const SizedBox(height: 10),
                             const Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: <Widget>[
                                 Text(
                                   "Total",
-                                  style: TextStyle(
-                                      color: Colors.black87,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600),
+                                  style: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.w600),
                                 ),
                                 Text(
                                   "\u20B9 600",
-                                  style: TextStyle(
-                                      color: Colors.black87,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600),
+                                  style: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.w600),
                                 ),
                               ],
                             ),
@@ -270,62 +195,7 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
                     ],
                   ),
                 ),
-                const Divider(
-                  height: 15,
-                  color: Colors.grey,
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          const Text(
-                            "Whom to book for?",
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 12,
-                                fontWeight: FontWeight.w600),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              const CircleAvatar(
-                                radius: 17,
-                                backgroundImage: NetworkImage(
-                                    'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80'),
-                              ),
-                              const SizedBox(
-                                width: 8,
-                              ),
-                              Text(
-                                "User 1",
-                                style: TextStyle(
-                                    color: Colors.grey[900],
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                      const Icon(
-                        Icons.check_circle,
-                        color: Colors.redAccent,
-                        size: 40,
-                      ),
-                    ],
-                  ),
-                ),
-                const Divider(
-                  height: 15,
-                  color: Colors.grey,
-                ),
+                const Divider(height: 15, color: Colors.grey),
               ],
             ),
           ),
