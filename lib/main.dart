85513840199fd65ca7024f8724082f8f29407877
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-02 22:14:06 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // 👈 Firebase Core Import
 import 'package:provider/provider.dart';
 import 'Provider/map_data_provider.dart';
 import 'screens/login_screen.dart';
-import 'screens/provider_registration_screen.dart'; // 👈 Import added
+import 'screens/provider_registration_screen.dart';
 import 'screens/category_screen.dart';
 import 'screens/mapsScreen.dart';
 import 'screens/appointmentScreen.dart';
 import 'screens/bookingScreen.dart';
 
-void main() {
+void main() async {
+  // Flutter engine initialization
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // 🔴 Genuine Firebase Initialization
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -33,7 +40,7 @@ class MyApp extends StatelessWidget {
         initialRoute: '/login',
         routes: {
           '/login': (context) => const LoginScreen(),
-          '/providerRegistration': (context) => const ProviderRegistrationScreen(), // 👈 Route added
+          '/providerRegistration': (context) => const ProviderRegistrationScreen(),
           '/': (context) => const CategoryScreen(),
           '/mappage': (context) => const Mappage(),
           '/appointmentScreen': (context) => const AppointmentScreen(),
