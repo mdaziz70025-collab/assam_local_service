4291b9f15a6cb2f6e92438806a396e858dfd84aa
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-03 21:10:21 +0530

    Update map_data_model.dart

--- lib/core/Data_models/map_data_model.dart
+++ lib/core/Data_models/map_data_model.dart
@@ -1,103 +1,90 @@
 import 'dart:convert';
 
-import 'package:meta/meta.dart';
-
-///Data Model for providing Initial data for Map viewing
-///fields are Location - for co-ordinates, name - name of the mark or shop,
-///rating - for rating provided, photo - image of the shop or profile,
-///workinghors- the open timing of the store
 class MapDataModel {
-    MapDataModel({
-        @required this.location,
-        @required this.name,
-        @required this.rating,
-        @required this.photo,
-        @required this.workingHours,
-    });
+  MapDataModel({
+    this.location,
+    this.name,
+    this.rating,
+    this.photo,
+    this.workingHours,
+  });
 
-    // Fields of the MapDataModel Class 
-    String location;
-    String name;
-    double rating;
-    dynamic photo;
-    WorkingHours workingHours;
+  String? location;
+  String? name;
+  double? rating;
+  dynamic photo;
+  WorkingHours? workingHours;
 
-    /// Factory of converting raw JSON string to the MapDataModel elements
-    factory MapDataModel.fromRawJson(String str) => MapDataModel.fromJson(json.decode(str));
+  factory MapDataModel.fromRawJson(String str) =>
+      MapDataModel.fromJson(json.decode(str));
 
-    /// Method of converting Model into Raw JSON String
-    String toRawJson() => json.encode(toJson());
+  String toRawJson() => json.encode(toJson());
 
-    /// Factory for converting individual JSON object into MapDataModel object
-    factory MapDataModel.fromJson(Map<String, dynamic> json) => MapDataModel(
-        location: json["Location"] == null ? null : json["Location"],
-        name: json["Name"] == null ? null : json["Name"],
-        rating: json["Rating"] == null ? null : json["Rating"].toDouble(),
+  factory MapDataModel.fromJson(Map<String, dynamic> json) => MapDataModel(
+        location: json["Location"],
+        name: json["Name"],
+        rating: json["Rating"] != null ? (json["Rating"] as num).toDouble() : null,
         photo: json["Photo"],
-        workingHours: json["WorkingHours"] == null ? null : WorkingHours.fromJson(json["WorkingHours"]),
-    );
+        workingHours: json["WorkingHours"] == null
+            ? null
+            : WorkingHours.fromJson(json["WorkingHours"]),
+      );
 
-    /// Converting individual MapDataModel object to a JSON object
-    Map<String, dynamic> toJson() => {
-        "Location": location == null ? null : location,
-        "Name": name == null ? null : name,
-        "Rating": rating == null ? null : rating,
+  Map<String, dynamic> toJson() => {
+        "Location": location,
+        "Name": name,
+        "Rating": rating,
         "Photo": photo,
-        "WorkingHours": workingHours == null ? null : workingHours.toJson(),
-    };
+        "WorkingHours": workingHours?.toJson(),
+      };
 
-    @override
-    String toString() {
-    return "${this.name}, ${this.location},${this.rating}";
-     }
+  @override
+  String toString() {
+    return "$name, $location, $rating";
+  }
 }
 
-///WorkingHours Class represents open hours of each day in a week individually
 class WorkingHours {
-    WorkingHours({
-        @required this.monday,
-        @required this.tuesday,
-        @required this.wednesday,
-        @required this.thursday,
-        @required this.friday,
-        @required this.saturday,
-        @required this.sunday,
-    });
+  WorkingHours({
+    this.monday,
+    this.tuesday,
+    this.wednesday,
+    this.thursday,
+    this.friday,
+    this.saturday,
+    this.sunday,
+  });
 
-    //Fields of WorkingHours class containing weekdays
-    String monday;
-    String tuesday;
-    String wednesday;
-    String thursday;
-    String friday;
-    String saturday;
-    String sunday;
+  String? monday;
+  String? tuesday;
+  String? wednesday;
+  String? thursday;
+  String? friday;
+  String? saturday;
+  String? sunday;
 
-    ///Factory to convert Raw JSON object into WorkingHours object
-    factory WorkingHours.fromRawJson(String str) => WorkingHours.fromJson(json.decode(str));
+  factory WorkingHours.fromRawJson(String str) =>
+      WorkingHours.fromJson(json.decode(str));
 
-    /// Converting WorkingHours object in Raw JSON String
-    String toRawJson() => json.encode(toJson());
+  String toRawJson() => json.encode(toJson());
 
-    /// Factory for converting individual JSON object into WorkingHours object
-    factory WorkingHours.fromJson(Map<String, dynamic> json) => WorkingHours(
-        monday: json["Monday"] == null ? null : json["Monday"],
-        tuesday: json["Tuesday"] == null ? null : json["Tuesday"],
-        wednesday: json["Wednesday"] == null ? null : json["Wednesday"],
-        thursday: json["Thursday"] == null ? null : json["Thursday"],
-        friday: json["Friday"] == null ? null : json["Friday"],
-        saturday: json["Saturday"] == null ? null : json["Saturday"],
-        sunday: json["Sunday"] == null ? null : json["Sunday"],
-    );
+  factory WorkingHours.fromJson(Map<String, dynamic> json) => WorkingHours(
+        monday: json["Monday"],
+        tuesday: json["Tuesday"],
+        wednesday: json["Wednesday"],
+        thursday: json["Thursday"],
+        friday: json["Friday"],
+        saturday: json["Saturday"],
+        sunday: json["Sunday"],
+      );
 
-    /// Converting individual WorkingHours object to a JSON object
-    Map<String, dynamic> toJson() => {
-        "Monday": monday == null ? null : monday,
-        "Tuesday": tuesday == null ? null : tuesday,
-        "Wednesday": wednesday == null ? null : wednesday,
-        "Thursday": thursday == null ? null : thursday,
-        "Friday": friday == null ? null : friday,
-        "Saturday": saturday == null ? null : saturday,
-        "Sunday": sunday == null ? null : sunday,
-    };
+  Map<String, dynamic> toJson() => {
+        "Monday": monday,
+        "Tuesday": tuesday,
+        "Wednesday": wednesday,
+        "Thursday": thursday,
+        "Friday": friday,
+        "Saturday": saturday,
+        "Sunday": sunday,
+      };
 }
