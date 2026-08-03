c650da79211f744306a221ede69dbd0574a2e0d8
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-03 21:09:58 +0530

    Update barber_data_from_map_model.dart

--- lib/core/Data_models/barber_data_from_map/barber_data_from_map_model.dart
+++ lib/core/Data_models/barber_data_from_map/barber_data_from_map_model.dart
@@ -1,35 +1,27 @@
 import 'dart:convert';
 
-import 'package:meta/meta.dart';
-
-import '../map_data_model.dart';
+import 'map_data_model.dart';
 import 'comment_model.dart';
-import 'service_mode.dart';
+import 'service_model.dart';
 
-///BarberDataFromMapModel class that contains Detailed information about the stores included in MapDataModel
-///Containers 4 fields MapData - MapDataModel object, services - List of Service object Instances, gallery - List of photos,
-///comments - List of Comment object Instances
 class BarberDataFromMapModel {
   BarberDataFromMapModel({
-    @required this.mapData,
-    @required this.services,
-    @required this.gallery,
-    @required this.comments,
+    this.mapData,
+    this.services,
+    this.gallery,
+    this.comments,
   });
 
-  MapDataModel mapData;
-  List<ServiceModel> services;
-  List<dynamic> gallery;
-  List<CommentModel> comments;
+  MapDataModel? mapData;
+  List<ServiceModel>? services;
+  List<dynamic>? gallery;
+  List<CommentModel>? comments;
 
-  /// Factory of converting raw JSON string to the BarberDataFromMapModel elements
   factory BarberDataFromMapModel.fromRawJson(String str) =>
       BarberDataFromMapModel.fromJson(json.decode(str));
 
-  /// Method of converting Model into Raw JSON String
   String toRawJson() => json.encode(toJson());
 
-  /// Factory for converting individual JSON object into BarberDataFromMapModel object
   factory BarberDataFromMapModel.fromJson(Map<String, dynamic> json) =>
       BarberDataFromMapModel(
         mapData: json["MapData"] == null
@@ -48,16 +40,16 @@ class BarberDataFromMapModel {
                 json["Comments"].map((x) => CommentModel.fromJson(x))),
       );
 
-  /// Converting individual BarberDataFromMapModel object to a JSON object
   Map<String, dynamic> toJson() => {
-        "MapData": mapData == null ? null : mapData.toJson(),
+        "MapData": mapData?.toJson(),
         "Services": services == null
             ? null
-            : List<dynamic>.from(services.map((x) => x.toJson())),
-        "Gallery":
-            gallery == null ? null : List<dynamic>.from(gallery.map((x) => x)),
+            : List<dynamic>.from(services!.map((x) => x.toJson())),
+        "Gallery": gallery == null
+            ? null
+            : List<dynamic>.from(gallery!.map((x) => x)),
         "Comments": comments == null
             ? null
-            : List<dynamic>.from(comments.map((x) => x.toJson())),
+            : List<dynamic>.from(comments!.map((x) => x.toJson())),
       };
 }
