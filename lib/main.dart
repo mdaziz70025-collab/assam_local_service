48436f26b052daaf1fec4ea23af2d8092ea00128
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-14 18:20:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+// 🔔 Notification Service Import
+import 'services/notification_service.dart';
+
 // Screens Imports
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
@@ -14,6 +17,10 @@ import 'screens/provider_registration_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // 🔔 Start Push Notifications & Local Alerts
+  await NotificationService.initialize();
+
   runApp(const MyApp());
 }
 
